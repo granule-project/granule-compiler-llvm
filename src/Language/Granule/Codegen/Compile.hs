2cb3e80c2b5d0885da0444eec07f4b47cfbacd60
Jacob Pake <github@pake.dev>
2025-04-01 14:41:30 +0100

    remove type stripping

--- src/Language/Granule/Codegen/Compile.hs
+++ src/Language/Granule/Codegen/Compile.hs
@@ -9,14 +9,12 @@ import Language.Granule.Codegen.ConvertClosures
 import Language.Granule.Codegen.Emit.EmitLLVM
 import Language.Granule.Codegen.MarkGlobals
 import Language.Granule.Codegen.RewriteAST
-import Language.Granule.Codegen.StripAST
 
 import qualified LLVM.AST as IR
 
 compile :: String -> AST () Type -> Either String IR.Module
 compile moduleName typedAST =
-  let stripped       = stripAST typedAST
-      rewritten      = rewriteAST stripped
+  let rewritten      = rewriteAST typedAST
       normalised     = normaliseDefinitions rewritten
       markedGlobals  = markGlobals normalised
       (Ok topsorted) = topologicallySortDefinitions markedGlobals
